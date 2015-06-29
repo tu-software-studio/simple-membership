2b6c4401d0e0df1804fb68fe7fd3aefd87c7d6e6
Justin Southworth <justin_southworth@taylor.edu>
2015-06-29 14:03:44 -0400

    Fix admin's account status being overridden on create.
        DONE:
         - Formerly, if "default-account-status" wasn't "Active", the
           admin's choice of account status would automatically be pending.
           This changes it so that it is now whatever the admin says.
    
        TODO:
         - Make sure that when a user creates their own account, it comes up
           as pending. (I'm pretty sure that calls a different function, but
           it isn't like I really know the codebase.)

--- classes/class.swpm-admin-registration.php
+++ classes/class.swpm-admin-registration.php
@@ -21,8 +21,13 @@ class SwpmAdminRegistration extends SwpmRegistration {
         $form = new SwpmForm($member);
         if ($form->is_valid()) {
             $member_info = $form->get_sanitized();
-            $account_status = SwpmSettings::get_instance()->get_value('default-account-status', 'active');
-            $member_info['account_state'] = $account_status;
+             /*
+              * -JS- Make it so that what the admin says regarding account status, goes when creating a new user.
+              */
+            if (!(SwpmSettings::get_instance()->get_value('default-account-status', 'active'))){
+                $account_status = SwpmSettings::get_instance()->get_value('default-account-status', 'active');
+                $member_info['account_state'] = $account_status;
+            }
             $plain_password = $member_info['plain_password'];
             unset($member_info['plain_password']);
             $wpdb->insert($wpdb->prefix . "swpm_members_tbl", $member_info);
